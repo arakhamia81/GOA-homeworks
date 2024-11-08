5afc7cc2e75516eda8ddce2af3a11f048e560652
arakhamia81 <nikoloziarakhamia@gmail.com>
2024-11-08 16:59:58 +0000

    second commit

---
+++ level 03/homework/homework.py
@@ -0,0 +1,76 @@
+# 3) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+name = "niko"
+lastname = "arakhamia"
+age = 10
+hob = "programing"
+country = "georgia"
+print(name)
+print(lastname)
+print(age)
+print(hob)
+
+print(country)
+
+
+# 4) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ დაამატეთ ერთმანეთზე.
+number = 50
+number2 = 10
+print(number + number2)
+
+
+# 5)  გააკეთეთ 10 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+# მიმატება
+print(1 + 2)
+print(10 + 15)
+print(23 + 7)
+print(100 + 200)
+print(5 + 5)
+print(0 + 12)
+print(8 + 3)
+print(50 + 30)
+print(7 + 14)
+print(60 + 40)
+# გაყოფა
+print(10 - 5)
+print(100 - 30)
+print(50 - 20) 
+print(15 - 7)
+print(60 - 40)
+print(25 - 25)
+print(80 - 10)
+print(45 - 15)
+print(90 - 33)
+print(120 - 60)
+# გამრავლება
+print(2 * 3)
+print(10 * 10)
+print(4 * 5)
+print(7 * 8) 
+print(3 * 15) 
+print(5 * 5)
+print(9 * 9) 
+print(1 * 100)
+print(6 * 7)
+print(8 * 11)
+# გაყოფა
+print(10 // 3)
+print(20 // 7)
+print(100 // 15)
+print(55 // 10)
+print(30 // 4)
+print(81 // 9)
+print(50 // 25)
+print(33 // 6)
+print(100 // 30)
+print(45 // 5)
+#ხარისხი
+print(2 ** 3)
+print(3 ** 2)
+print(5 ** 3)
+print(4 ** 4)
+print(6 ** 2)
+print(7 ** 3)
+print(2 ** 10)
+print(1 ** 5)
+print(10 ** 2)
+print(3 ** 4)
---
+++ level 05/classwork/classwork.py
@@ -0,0 +1,48 @@
+# გამოასწორეთ ხარვეზები
+# 1
+greeting = "Hello"
+userName = "student"
+age = 20
+location = "New York"
+
+#2
+print('Welcome, ' + userName + "!" ) 
+print(greeting + ", " + userName + "!") 
+
+#3
+print("Your age is ", age)  
+print("In 5 years, you will be ", age + 5, " years old") 
+
+#4
+favoriteNumber = 7
+print("Your favorite number doubled is ", favoriteNumber * 7)  
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = total_score + bonus_points
+print("Your final score is ", finalScore)
+
+#6
+days_in_a_week = 4
+print("There are ", days_in_a_week * 4, " days in four weeks")
+
+#7
+firstName = "John"
+print("Hello, " + firstName + "!")
+
+#8
+nickname = "Ace"
+print("Your nickname is: " + nickname)
+
+#9
+print("The length of your location is " + location)
+
+#10
+year = 2024
+print("Next year is ", year + 1)
+print("Your age next year will be ", age + 1)
+
+#11
+farewell = "Goodbye"
+print(farewell, userName + "! See you next time.")
